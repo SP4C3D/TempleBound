0e94f4de2b2aee8abaf670d9abaa28ffef030183
Sebastian Benaya <98514080+SP4C3D@users.noreply.github.com>
2025-05-24 12:41:11 +0700

    Ubah starting page ke app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<FrontPage />} />
+        <Route path="/" element={<App />} />
         <Route path="/TempleBound" element={<TempleBound />} />
       </Routes>
     </BrowserRouter>
