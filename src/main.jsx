989b36104a1bf46afec4748ff73e61cdbbc69444
Sebastian Benaya <98514080+SP4C3D@users.noreply.github.com>
2025-05-25 11:09:56 +0700

    Gw lupa import app di main.jsx lmao

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import FrontPage from "./FrontPage";
+import App from "./App";
 import TempleBound from "./TempleBound"; // Create this next
 
 ReactDOM.createRoot(document.getElementById("root")).render(
