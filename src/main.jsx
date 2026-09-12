db3fd7b08fb204fd7075995df26d8a1e627aced8
butterscotchh <sampansedang@gmail.com>
2026-09-12 22:35:56 +0700

    vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import TempleBound from "./TempleBound";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/TempleBound" element={<TempleBound />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
